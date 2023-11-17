cd94946f31a04ec4b6e5528f8df6a7453075f943
Nguyễn Hoàng Anh Tuấn <tuantoquq@gmail.com>
2023-11-17 12:10:00 +0700

    wedding-02: fix intro component size

--- components/intro/Item.tsx
+++ components/intro/Item.tsx
@@ -21,7 +21,7 @@ export default function Item({
   instagram,
 }: ItemProps) {
   return (
-    <div className="flex-auto flex flex-col relative justify-center items-center">
+    <div className="basis-1/2 h-full flex flex-col relative justify-center items-center">
       <Image
         className="xs:w-64 xs:h-64 sm:w-80 sm:h-80 rounded-full border-4 border-cs-green-700 z-10 object-center object-cover"
         src={image}
@@ -29,11 +29,13 @@ export default function Item({
         alt=""
       />
 
-      <div className="bg-cs-green-100 relative xs:-mt-32 sm:-mt-40 xs:pt-36 sm:pt-48 pb-10 md:px-10 px-5 rounded-2xl flex flex-col gap-2 md:gap-5">
+      <div className="bg-cs-green-100 relative xs:-mt-32 sm:-mt-40 xs:pt-36 sm:pt-48 pb-10 md:px-10 px-5 rounded-2xl flex flex-col gap-2 md:gap-5 h-full">
         <Typography className="font-medium" variant="h2">
           {name}
         </Typography>
-        <Typography variant="desc">{description}</Typography>
+        <Typography variant="desc" className="h-[6rem]">
+          {description}
+        </Typography>
         <div className="flex flex-row justify-center items-center gap-2">
           <Link href={`${facebook}`}>
             <FaFacebook className="text-3xl text-secondary-cl" />
